43bc5de4700616e238b8c24b54daebc73e2da2ed
Aladdin Sonni <aladdin.sonni@gmail.com>
2024-05-01 20:36:58 +0200

    Create New Expense

--- frontend/src/routes/create-expense.tsx
+++ frontend/src/routes/create-expense.tsx
@@ -1,7 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
+
 const CreateExpense = () => {
-  return <div className="p-2">Create a new expense</div>;
+  return (
+    <div className="p-2">
+      <h2>Create a new expense</h2>
+      <form className="max-w-xl m-auto">
+        <Label htmlFor="title">Title</Label>
+        <Input type="text" id="title" placeholder="Title" />
+        <Label htmlFor="amount">Amount</Label>
+        <Input type="text" id="amount" placeholder="Amount" />
+        <Button className="mt-4" type="submit">
+          Create Expense
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export const Route = createFileRoute('/create-expense')({
